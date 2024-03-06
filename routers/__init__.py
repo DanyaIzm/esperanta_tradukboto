a81b9c6a59ed25a3867aae0d06dbd098e0ba6bfd
Daniel <danizmd8@gmail.com>
2024-03-06 16:44:44 +0300

    feat: added dictionary functionality to bot

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,10 +1,12 @@
 from aiogram import Dispatcher
 from .common_router import router as common_router
 from .search_router import router as search_router
+from .dictionary_router import router as dictionary_router
 from middlewares.session_middleware import SessionMiddleware
 
 
 def register_all_routers(dp: Dispatcher) -> None:
     search_router.message.middleware(SessionMiddleware())
+    dictionary_router.message.middleware(SessionMiddleware())
 
-    dp.include_routers(common_router, search_router)
+    dp.include_routers(common_router, search_router, dictionary_router)
---
+++ routers/dictionary_router.py
@@ -0,0 +1,60 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiohttp import ClientSession
+from exceptions import AnswerTooLargeException
+from scrapper import Dictionary
+from aiogram.exceptions import TelegramEntityTooLarge, TelegramBadRequest
+
+
+router = Router()
+
+
+@router.message()
+async def command_f_handler(message: Message, session: ClientSession):
+    try:
+        dictionary = Dictionary(session)
+
+        if len(message.text) <= 1:
+            raise AnswerTooLargeException("Mesaĝo estos tro granda por Telegram")
+
+        cleaned_text = message.text.strip().lower()
+
+        result_html = await dictionary.get_html(cleaned_text)
+
+        if len(result_html) > 4096 * 4:
+            raise AnswerTooLargeException("Respondo estos pli ol 4 mesaĝoj")
+
+        # https://github.com/aiogram/aiogram/discussions/963#discussioncomment-3144036
+        if len(result_html) > 4096:
+            parts = []
+            while result_html:
+                if len(result_html) <= 4096:
+                    parts.append(result_html)
+                    break
+
+                part = result_html[:4096]
+                first_ln = part.rfind("\n")
+
+                if first_ln != -1:
+                    new_part = part[:first_ln]
+                    parts.append(new_part)
+                    result_html = result_html[first_ln + 1 :]
+                else:
+                    first_space = part.rfind(" ")
+
+                    if first_space != -1:
+                        new_part = part[:first_space]
+                        parts.append(new_part)
+                        result_html = result_html[first_space + 1 :]
+                    else:
+                        parts.append(part)
+                        result_html = result_html[4096:]
+
+            for part in parts:
+                await message.answer(text=part)
+        else:
+            await message.answer(text=result_html)
+    except (TelegramEntityTooLarge, AnswerTooLargeException, TelegramBadRequest):
+        error_text = f"Ŝajnas ke la mesaĝo estas tro granda. Bonvolu sendi vorto kun pli da literojn ol kiom vi sendis jam."
+
+        await message.answer(text=error_text)
